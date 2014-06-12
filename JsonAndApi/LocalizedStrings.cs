dc49ed78c835dca65fd22a9fbe5f0ff075116764
dev_ankit <ankit_khullar@live.in>
2014-06-12 21:55:19 +0530

    Parsing JSON and Consuming Web API

---
+++ JsonAndApi/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using JsonAndApi.Resources;
+
+namespace JsonAndApi
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ JsonAndApi/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using JsonAndApi.Resources;
+using Newtonsoft.Json.Linq;
+
+namespace JsonAndApi
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private async void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //    JObject jObj = JObject.Parse(InputBox.Text);
+            //    MessageBox.Show(jObj["name"].ToString());
+            //    MessageBox.Show(jObj["lang"][0].ToString());
+            Uri url = new Uri(InputBox.Text, UriKind.Absolute);
+            HttpClient client = new HttpClient();
+            String response = await client.GetStringAsync(url);
+            var jObj = JObject.Parse(response);
+            MessageBox.Show(jObj["gender"].ToString());
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
